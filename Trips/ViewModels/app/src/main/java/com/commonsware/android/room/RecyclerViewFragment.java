f6eec853c460e27b0b7c1bdf8e03c37185d6145f
Mark Murphy <mmurphy@commonsware.com>
2018-05-13 13:50:45 -0400

    update dependencies, move to fragment backport

--- Trips/ViewModels/app/src/main/java/com/commonsware/android/room/RecyclerViewFragment.java
+++ Trips/ViewModels/app/src/main/java/com/commonsware/android/room/RecyclerViewFragment.java
@@ -14,14 +14,14 @@
 
 package com.commonsware.android.room;
 
-import android.arch.lifecycle.LifecycleFragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class RecyclerViewFragment extends LifecycleFragment {
+public class RecyclerViewFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
